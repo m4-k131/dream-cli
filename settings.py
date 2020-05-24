96e2fc8a5f1926ef5bc0f0bb1ab7067dc3facce1
m4-k131 <40889634+m4-k131@users.noreply.github.com>
2020-05-24 16:54:07 +0200

    Add files via upload

--- settings.py
+++ settings.py
@@ -5,16 +5,6 @@ Created on Sat Jan  4 12:55:31 2020
 """
 import os
 import numpy as np
-def write_all(layername):
-    rend=get_default_renderer()
-    rend['layer']=layername
-    for file in os.listdir():
-        splitted=file.split('_')
-        rend['f_channel']=int(splitted[1])
-        rend['l_channel']=int(splitted[2].split('.')[0])
-        save_renderer(rend, layername+'_'+str(rend['l_channel'])+'.json')
-
-import json
 
 def get_default_setting():
     setting_dict={
@@ -36,10 +26,10 @@ def get_default_setting():
 def get_default_renderer():
     renderer_dict={
         'name':"default_renderer",
-        'layer':"layer",
+        'layer':"conv2d1_pre_relu",
         'f_channel':0,
-        'l_channel':1,
-        'max_channel':1,
+        'l_channel':64,
+        'max_channel':64,
         'squared':True,
         'cropped':False,
         'boundraries':[[0,1],[0,1]],
@@ -56,27 +46,6 @@ def get_default_renderer():
         't_masks':[]
         }
     return renderer_dict
-def save_renderer(r, filename):
-    to_file=r
-    
-    #r['mask']=r['mask'].tolist()
-    with open(filename, "w") as fp:
-        json.dump(to_file, fp, indent=4)
-
-
-def save_setting(s, filename):
-    to_file=s
-    for renderer in to_file['renderer']:
-        renderer=renderer.tolist()
-    with open(filename, 'w') as fp:
-        json.dump(to_file, fp, indent=4)
-        
-
-def load_renderer(filename):
-    with open(filename, 'r') as fp:
-        data=json.load(fp)
-    return data
-
 
 orig_image_name="No image selected"
 orig_image=[]
